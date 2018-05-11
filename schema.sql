d9933ec4822025a5f4aff5919f304cf4edeb6308
Olmo Ziarko <olmo@ziarko.be>
2018-05-11 02:35:20 +0200

    new schema

--- schema.sql
+++ schema.sql
@@ -1,12 +1,20 @@
 CREATE TABLE users (
     id          INTEGER PRIMARY KEY,
     username    TINYTEXT,
-    password    TINYTEXT,
-    parent      INTEGER REFERENCES users (id) ON DELETE CASCADE,
+    hash        TINYTEXT,
+
+    invitation  INTEGER REFERENCES invitations (id) ON DELETE CASCADE,
     bio         TINYTEXT
 );
 
 CREATE TABLE invitations (
+    id          INTEGER PRIMARY KEY,
     userid      INTEGER REFERENCES users (id) ON DELETE CASCADE,
-    code        TINYTEXT
+    code        TINYTEXT,
+    used        BOOLEAN DEFAULT 0
 );
+
+INSERT INTO users (username, hash, bio) VALUES ("root", "x", "the start of it all");
+INSERT INTO invitations (userid, code) VALUES (1, "yes");
+INSERT INTO invitations (userid, code) VALUES (1, "welcome");
+INSERT INTO invitations (userid, code) VALUES (1, "please enter");
